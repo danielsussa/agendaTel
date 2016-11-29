0d5c4f17d56f0c3ad9307ce77504d365d05d181d
Daniel Kanczuk <dekanczuk@stefanini.com>
2016-11-29 14:38:26 -0200

    primeiro commit

---
+++ src/main/java/br/com/agendatel/AgendaTelApplication.java
@@ -0,0 +1,12 @@
+package br.com.agendatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaTelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaTelApplication.class, args);
+	}
+}
